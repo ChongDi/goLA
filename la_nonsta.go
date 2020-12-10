61d348bdf2a621ceb9dcbd00a85c3f1363ac440a
chongdi <dichong95@foxmail.com>
2020-12-10 07:50:50 +0000

    init

---
+++ la_nonsta.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	utils "LA/utils"
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println(utils.Test()) // keep utils in import
+}
---
+++ utils/la_basics.go
@@ -0,0 +1,6 @@
+package utils
+
+// Test returns 1
+func Test() int {
+	return 1
+}
